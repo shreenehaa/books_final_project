7730d35ef05eeb1c39a953ab7d4042344e35d682
shreenehaa <shreenehaa2003@gmail.com>
2024-02-13 10:15:20 +0530

    imported cors

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 import express from "express"; // "type": "module"
 import { sequelize } from "./config.js";
 import moviesRouter from "./routes/movies-routes.js";
+import cors from "cors";
+import userRouter from "./routes/user-routes.js";
 try {
   await sequelize.authenticate();
   await sequelize.sync();
@@ -12,9 +14,11 @@ try {
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT;
+app.use(cors());
 app.get("/", function (request, response) {
   response.send("🙋‍♂️, 🌏 🎊✨🤩");
 });
-app.use("/movies", moviesRouter);
+// app.use("/movies", moviesRouter);
+app.use("/users", userRouter);
 
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
